9cc8dc153e4763af6d238667f4e7696cd4535972
Luis Sotomayor <LSotomayor@zynx.com>
2013-11-11 22:45:06 -0800

    Added code to retrieve first name in table

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoNetSpike2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "select * from [SalesLT].[Customer] where CustomerID=@id";
+
+                    var parameter = command.CreateParameter();
+                    parameter.ParameterName = "@id";
+                    parameter.Value = 1;
+                    command.Parameters.Add(parameter);
+
+                    var reader = command.ExecuteReader();
+
+                    if (!reader.Read())
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data found");
+                    }
+
+                    var firstName = reader.GetString(3);
+                    Console.WriteLine("First name: {0}", firstName);
+                }
+            }
+        }
+    }
+}
